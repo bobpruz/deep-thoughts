723d299a8cc814fbc3b0d80bfcf6f227cc2689f0
bobpruz <bobpruz@gmail.com>
2022-06-26 06:46:58 -0500

    class progress

--- client/src/pages/Profile.js
+++ client/src/pages/Profile.js
@@ -1,23 +1,37 @@
 import React from "react";
-import { useParams } from "react-router-dom";
-
 import ThoughtList from "../components/ThoughtList";
 import FriendList from "../components/FriendList";
 
 import { useQuery } from "@apollo/client";
-import { QUERY_USER } from "../utils/queries";
+import { QUERY_USER, QUERY_ME } from "../utils/queries";
+import Auth from "../utils/auth";
+import { Navigate, useParams } from "react-router-dom";
 
 const Profile = () => {
   const { username: userParam } = useParams();
 
-  const { loading, data } = useQuery(QUERY_USER, {
+  const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
     variables: { username: userParam },
   });
 
-  const user = data?.user || {};
+  const user = data?.me || data?.user || {};
+
+  // navigate to personal profile page if username is the logged-in user's
+  if (Auth.loggedIn() && Auth.getProfile().data.username === userParam) {
+    return <Navigate to="/profile" />;
+  }
 
   if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (!user?.username) {
+    return (
+      <h4>
+        You need to be logged in to see this page. Use the navigation links
+        above to sign up or log in!
+      </h4>
+    );
   }
 
   return (
